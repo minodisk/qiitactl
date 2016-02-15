c9969ee1cd3cc48486cb51e2c0be684c3b7430c7
minodisk <daisuke.mino@gmail.com>
2016-02-16 01:42:15 +0900

    Add release flow

---
+++ release.sh
@@ -0,0 +1,21 @@
+if [ "$CIRCLE_CI" == "true" ]; then
+  sudo wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  sudo mv jq-linux64 /usr/bin/jq
+  sudo chmod a+x /usr/bin/jq
+  sudo rm -rf jq-linux64
+  jq --version
+fi
+
+version=`cat .goxc.json | jq -r ".PackageVersion"`
+has_version=`curl https://api.github.com/repos/minodisk/qiitactl/tags | jq "\
+  any( \
+    .name == \"v$version\" \
+  ) \
+"`
+if [ "$has_version" == "true" ]; then
+  echo "v$version is already released"
+  exit 1
+fi
+
+goxc -wlc default publish-github -apikey=$GITHUB_TOKEN
+goxc
